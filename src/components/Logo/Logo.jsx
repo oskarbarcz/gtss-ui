679ea7c495051b2aa5c7ef6a2171ac72b85bc722
Oskar Barcz <oskar.barcz@archi-tektur.pl>
2023-03-02 23:18:23 +0100

    Add base view of sign in screen (#1)

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import './Logo.css'
+
+export default function Logo() {
+  return (
+    <div className="logo m-4">
+      <span className="logo__text logo__text--black">AT</span>
+      <span className="logo__text logo__text--orange">Cloud</span>
+    </div>
+  )
+}
